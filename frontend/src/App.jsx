159f187c1c467a7188e98dbc9374021b5c3a4489
kgaurav8026 <kumeriya.gaurav@gmail.com>
2024-09-14 00:21:00 +0530

    added AuthContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
-import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 function App() {
+  const { authUser } = useAuthContext();
   return (
-    <div className="p-4 h-screen flex items-center justify-center ">
-      {/* <SignUp /> */}
-      <Home />
+    <div>
+      <Toaster />
+      <div className="p-4 h-screen flex items-center justify-center ">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/" /> : <SignUp />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
